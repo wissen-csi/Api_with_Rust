3766d3c312c6b7985c4f68ee49cb21a70a49bb2a
Jorge <wissencsi@gmail.com>
2026-03-14 00:25:01 -0500

    add the runtime tokio with use the programming Asynchronous Programming

---
+++ api/target/debug/deps/api-20c6011b14f4cda3.d
@@ -0,0 +1,5 @@
+C:\Users\usuario\Desktop\rust\repo\Api_with_Rust\api\target\debug\deps\api-20c6011b14f4cda3.d: src\main.rs
+
+C:\Users\usuario\Desktop\rust\repo\Api_with_Rust\api\target\debug\deps\libapi-20c6011b14f4cda3.rmeta: src\main.rs
+
+src\main.rs:
---
+++ api/target/debug/deps/api-fd838c254acda7fd.d
@@ -0,0 +1,5 @@
+C:\Users\usuario\Desktop\rust\repo\Api_with_Rust\api\target\debug\deps\api-fd838c254acda7fd.d: src\main.rs
+
+C:\Users\usuario\Desktop\rust\repo\Api_with_Rust\api\target\debug\deps\libapi-fd838c254acda7fd.rmeta: src\main.rs
+
+src\main.rs:
